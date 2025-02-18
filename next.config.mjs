745fd2a46a1b98e59873c6ac5e2eb5a81185181a
Samarth Kamble <kamblesamarth04@gmail.com>
2025-02-19 02:09:43 +0530

    Clearing the repo

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [{ hostname: "firebasestorage.googleapis.com" }],
-  },
-};
-
-export default nextConfig;
